415889bc78b09594c74ed347a9c1e5486f11e1b4
Jonysegal <jonysegal@gmail.com>
2021-08-03 02:02:28 -0400

    Add ui framework

---
+++ Hearthstone Deck Tracker/Controls/Overlay/AchievementProgressIndicator.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Hearthstone_Deck_Tracker.Annotations;
+using Hearthstone_Deck_Tracker.BobsBuddy;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace Hearthstone_Deck_Tracker.Controls.Overlay
+{
+	/// <summary>
+	/// Interaction logic for HeroOptionGlobalStats.xaml
+	/// </summary>
+	public partial class AchievementProgressIndicator : UserControl, INotifyPropertyChanged
+	{
+		public AchievementProgressIndicator()
+		{
+			InitializeComponent();
+		}
+
+		private Visibility _inProgressVisibility = Visibility.Visible;
+		public Visibility InProgressVisibility
+		{
+			get => _inProgressVisibility;
+			set
+			{
+				_inProgressVisibility = value;
+				OnPropertyChanged();
+			}
+		}
+
+		private Visibility _earnedVisibility = Visibility.Collapsed;
+		public Visibility EarnedVisibility
+		{
+			get => _earnedVisibility;
+			set
+			{
+				_earnedVisibility = value;
+				OnPropertyChanged();
+			}
+		}
+		public event PropertyChangedEventHandler PropertyChanged;
+		[NotifyPropertyChangedInvocator]
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
---
+++ Hearthstone Deck Tracker/Controls/Overlay/AchievementTextPanel.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Hearthstone_Deck_Tracker.Annotations;
+using Hearthstone_Deck_Tracker.BobsBuddy;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace Hearthstone_Deck_Tracker.Controls.Overlay
+{
+	/// <summary>
+	/// Interaction logic for HeroOptionGlobalStats.xaml
+	/// </summary>
+	public partial class AchievementTextPanel : UserControl, INotifyPropertyChanged
+	{
+		public AchievementTextPanel()
+		{
+			InitializeComponent();
+		}
+
+		private string _achievementText;
+		public string AchievementText
+		{
+			get => _achievementText;
+			set
+			{
+				_achievementText = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		[NotifyPropertyChangedInvocator]
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+
+}
